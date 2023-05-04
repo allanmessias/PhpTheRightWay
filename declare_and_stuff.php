1647921a887c79133d45c638b18da0fd436a423f
unknown <allanmessiasdecoelho@gmail.com>
2023-05-04 14:07:16 -0300

    just revising stuff

---
+++ declare_and_stuff.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1); 
+
+
+function sum(int $a, int $b): int {
+    return $a + $b;
+}
+
+echo sum(10, 10);
+
+
+// declare - enconding
+
+
+
+  
+
+// declare - strict_types
+
+
+
---
+++ include.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+require 'declare_and_stuff.php';
+
+echo sum(5, 20); 
